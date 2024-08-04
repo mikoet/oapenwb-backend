3a6ae7aa882096a9933084cb37c5caffcfea3965
mikoet <mkoether38@gmail.com>
2024-08-04 20:01:30 +0200

    Added new submodule project oapenwb-spring. It does not run yet. There are incompatibilities with oapenwb-javalin due to different hibernate version (and maybe more).

---
+++ oapenwb-spring/src/main/java/dk/ule/oapenwb2/DictSpringApplication.java
@@ -0,0 +1,12 @@
+package dk.ule.oapenwb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictSpringApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(DictSpringApplication.class, args);
+	}
+}
---
+++ oapenwb-spring/src/test/java/dk/ule/oapenwb2/DictSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package dk.ule.oapenwb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictSpringApplicationTests
+{
+	@Test
+	void contextLoads() {
+	}
+}
