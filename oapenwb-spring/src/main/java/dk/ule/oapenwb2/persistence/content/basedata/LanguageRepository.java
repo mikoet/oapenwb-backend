1c9d2bb91cfe5f9f7e08a6cfedd389beccb3f791
mikoet <mkoether38@gmail.com>
2024-09-05 23:07:59 +0200

    oapenwb-spring: - Corrected repository name.

--- oapenwb-spring/src/main/java/dk/ule/oapenwb2/persistence/content/basedata/LanguageRepository.java
+++ oapenwb-spring/src/main/java/dk/ule/oapenwb2/persistence/content/basedata/LanguageRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LanguageRepositories extends JpaRepository<Language, Integer>
+public interface LanguageRepository extends JpaRepository<Language, Integer>
 {
 }
